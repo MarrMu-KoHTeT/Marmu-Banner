07e43b53cece404247d0495edc1e44202308af6d
MarrMu-KoHTeT <85302604+MarrMu-KoHTeT@users.noreply.github.com>
2021-10-08 23:31:48 +0630

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#
+# Termux Banner
+# Banner by MarMu
+# _____Do Not Copy Or Modify_____
+-command_not_found_handle() {
+	
+}
+clear
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+#echo -e $red
+#echo -e "hack"
+#echo -e $green
+#echo -e "((__---------__)) $green          New          $green ((__---------__))"
+#echo -e $red
+#echo
+#echo -e "    |||||||||             $green  FOR  $red             |||||||||"
+#echo
+#echo
+#echo -e $yellow" ---------------($red  T E R M U X  M Y A N M A R $yellow)---------------"
+#echo -e $yellow
+figlet -f big "     MarMu" | lolcat
+echo " "
+echo -e $yellow"☆☆☆" $red "Love Myanmar $white"$yellow ☆☆☆
+echo -e $purple           ♡$yellow TERMUX$white of$green MYANMAR$purple ♡ "
+echo -e $green"    ☜===M$red====A$yellow====R$white====M$purple====U$green===☞ "
+echo
+#PS1='\[\033[01;32m\]┌────\[\033[01;33m\][\[\033[01;32m\]MarMu \[\033[01;31m\]@ \[\033[01;35m\]${PWD/*\//}\[\033[01;33m\]]\033[1;33m──────────────────────\033[1;30m[\T]\e[1;33m─┐\n\e[1;33m\n└>\e[1;31m # '
+PS1='\[\033[01;33m\]┌─\[\033[01;36m\]《\[\033[01;75m\]ZAKI\[\033[01;31m\] @ \[\033[01;33m\]${PWD/*\//}\[\033[01;36m\]》\[\033[01;32m\]☆\[\033[01;33m\]L♡ve.U.All\[\033[01;31m\] @ \[\033[01;32m\]\T\[\033[01;35m\]☆ \[\033[01;36m\]{\[\033[01;32m\]2021\033[01;36m\]}\[\033[01;32m\]*\[\033[01;36m\]\[\033[01;32m\]\[\033[01;35m\]
+\[\033[01;33m\]|
+\[\033[01;33m\]╰( ･ ᗜ ･ )☞M\[\033[01;32m\]☞☞\[\033[01;33m\] '
+#PS1='\[\031[01;33m\]┌───\[\033[01;35m\][ \[\033[01;32m\]MYANMAR\[\033[01;31m\] @ \[\033[01;32m\]${PWD/*\//}\[\033[01;33m\] ]\033[1;33m────\[\033[01;33m\][ \[\033[01;32m\]2018 \[\033[01;33m\]]\[\033[01;33m\]$
+#\[\033[01;31m\]¦
+#\[\033[01;31m\]┗(ಠ_ಠ)☞\[\033[01;35m\] '
+alias c='clear'
+alias x='exit'
+alias h='help'
+alias dt='rm -rf'
