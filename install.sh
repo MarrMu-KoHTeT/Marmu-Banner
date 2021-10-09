fa14ae5ba2f6d3ade713b884bc85331e7d2f0e7a
MarrMu-KoHTeT <85302604+MarrMu-KoHTeT@users.noreply.github.com>
2021-10-09 19:44:28 +0630

    install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,20 @@ figlet Basic Installation
 apt install toilet -y
 apt install cowsay -y
 apt install nano -y
-apt install ruby -y
 gem install lolcat
+echo $W "Installing requirements........"$G
+apt-get install curl -y
+apt-get install tor -y
+apt-get install perl -y
+apt-get install git -y
+apt-get install hydra -y
+apt-get install python -y
+apt-get install python2 -y
+apt-get install php -y
+apt-get install nmap -y
+apt-get install apache2 -y 
+apt-get install cowsay -y
+apt-get install ruby -y
 figlet -f small Love U All!!! | lolcat
 echo
 echo -e "\e[1m Now Run ..!!!"
@@ -20,11 +32,9 @@ echo
 echo -e "\e[1m\e[32m Creation by :\e[33m MarMu (Myanmar)"
 echo
 cmatrix
-a
-b
-c
-d
-e
-f
+
+
+
+
 echo
 exit
