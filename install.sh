67d13471fdb0c0d41be681a2a2ed1c91036bb7f1
MarrMu-KoHTeT <85302604+MarrMu-KoHTeT@users.noreply.github.com>
2021-10-09 15:00:18 +0630

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo 
+echo -e "\e[1m 🎭 Love u All..!!!"
+apt update
+apt upgrade -y
+apt install cmatrix
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f small Love U All!!! | lolcat
+echo
+echo -e "\e[1m Now Run ..!!!"
+echo
+echo -e "\e[1m\e[32m Creation by :\e[33m MarMu (Myanmar)"
+echo
+cmatrix
