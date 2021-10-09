94167ae9f3e10dfc15f9c18a72cfdd22d02bb531
MarrMu-KoHTeT <85302604+MarrMu-KoHTeT@users.noreply.github.com>
2021-10-09 15:51:11 +0630

    Update and rename zaki.sh to myanmar.sh

---
+++ myanmar.sh
@@ -0,0 +1,52 @@
+-command_not_found_handle() {
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+clear
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+#echo -e $red
+#echo -e "hack"
+#echo -e $green
+#echo -e "((__---------__)) $green          New          $green ((__---------__))"
+#echo -e "   (_)$red O O $green(_)                               (_)$red O O $green(_)"
+#echo -e "      [$red - $green]                                     [$red - $green]"
+#echo -e "       o_o           ($yellow  Stay strong  $green)           o_o"
+#echo -e $red
+#echo
+#echo -e "    |||||||||             $green  FOR  $red             |||||||||"
+#echo
+#echo
+#echo -e $yellow" ---------------($red  T E R M U X  $yellow)---------------"
+#echo -e $yellow
+figlet -f big "     MarMu" | lolcat
+echo " "
+echo -e $red"      -----☆" $red "love you all $white"$yellow ☆-----
+echo -e $purple"           《$red T$green E$yellow R$red M$green U$yellow X$white of$green MYANMAR$purple 》"
+echo -e $yellow"        ☆☆☆" $green<====M$red=====A$yellow=====R$purple====M$white===U$blue===>"$yellow ☆☆☆""
+echo
+#PS1='\[\033[01;32m\]┌────\[\033[01;33m\][\[\033[01;32m\]marmu \[\033[01;31m\]@ \[\033[01;35m\]${PWD/*\//}\[\033[01;33m\]]\033[1;33m──────────────────────\033[1;30m[\T]\e[1;33m─┐\n\e[1;33m\n└>\e[1;31m # '
+PS1='\[\033[01;33m\]┌─\[\033[01;36m\]《\[\033[01;75m\]MARMU\[\033[01;31m\] @ \[\033[01;33m\]${PWD/*\//}\[\033[01;36m\]》\[\033[01;32m\]☆\[\033[01;36m\]《\[\033[01;32m\]♡.U.All\[\033[01;31m\] @ \[\033[01;32m\]\T\[\033[01;36m\]》\[\033[01;32m\]☆ \[\033[01;36m\]{\[\033[01;31m\]2021\033[01;36m\]}\[\033[01;32m\]*\[\033[01;36m\]\[\033[01;32m\]\[\033[01;35m\]
+\[\033[01;33m\]|
+\[\033[01;33m\]╰━☞M\[\033[01;32m\]»»☞\[\033[01;33m\] '
+#PS1='\[\031[01;33m\]┌───\[\033[01;35m\][ \[\033[01;32m\]MarMu\[\033[01;31m\] @ \[\033[01;32m\]${PWD/*\//}\[\033[01;33m\] ]\033[1;33m────\[\033[01;33m\][ \[\033[01;32m\]2018 \[\033[01;33m\]]\[\033[01;33m\]$
+#\[\033[01;31m\]¦
+#\[\033[01;31m\]└─━☞\[\033[01;35m\] '
+alias c='clear'
+alias x='exit'
+alias h='help'
+alias ip='curl ifconfig.me'
+alias ifc='ifconfig wlan0'
+alias g='git clone'
+alias pk='pkg update && pkg upgrade'
+alias n='nano'
+alias l='ls -f'
+
+
+
+
+
