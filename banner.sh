491ddb7ff7ddada28c3e99ecfe713e5e2b8c1d8f
MarrMu-KoHTeT <85302604+MarrMu-KoHTeT@users.noreply.github.com>
2021-10-09 14:14:38 +0630

    banner.sh

--- banner.sh
+++ banner.sh
@@ -1,6 +1,7 @@
 # Termux Banner
 # Banner by MarMu
 # _____Do Not Copy Or Modify_____
+
 -command_not_found_handle() {
 	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
 }
@@ -26,8 +27,8 @@ white='\e[1;37m'
 figlet -f big "     MarMu" | lolcat
 echo " "
 echo -e $yellow"☆☆☆" $red "Love Myanmar $white"$yellow ☆☆☆
-echo -e $purple           ♡$yellow TERMUX$white of$green MYANMAR$purple ♡ "
-echo -e $green"    ☜===M$red====A$yellow====R$white====M$purple====U$green===☞ "
+echo -e $purple           ♡$yellow TERMUX$white of$green MYANMAR$purple ♡"
+echo -e $green"    ☜===M$red====A$yellow====R$white====M$purple====U$green===☞"
 echo
 #PS1='\[\033[01;32m\]┌────\[\033[01;33m\][\[\033[01;32m\]MarMu \[\033[01;31m\]@ \[\033[01;35m\]${PWD/*\//}\[\033[01;33m\]]\033[1;33m──────────────────────\033[1;30m[\T]\e[1;33m─┐\n\e[1;33m\n└>\e[1;31m # '
 PS1='\[\033[01;33m\]┌─\[\033[01;36m\]《\[\033[01;75m\]ZAKI\[\033[01;31m\] @ \[\033[01;33m\]${PWD/*\//}\[\033[01;36m\]》\[\033[01;32m\]☆\[\033[01;33m\]L♡ve.U.All\[\033[01;31m\] @ \[\033[01;32m\]\T\[\033[01;35m\]☆ \[\033[01;36m\]{\[\033[01;32m\]2021\033[01;36m\]}\[\033[01;32m\]*\[\033[01;36m\]\[\033[01;32m\]\[\033[01;35m\]
@@ -46,3 +47,4 @@ alias dt='rm -rf'
 
 sleep 2
 exit
+done
